9863c5782e25c24f086c0ab6d0338e5333312db1
jordmundgand <109611636+jordmundgand@users.noreply.github.com>
2024-02-10 14:47:28 +0700

    Update losses.py

--- tensorflow_murmur/losses/losses.py
+++ tensorflow_murmur/losses/losses.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 def masked_loss(label, pred):
+  '''Classical transformers masked SparseCategoricalCrossentropy loss function.'''
   mask = label != 0
   loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
     from_logits=False, reduction='none')
