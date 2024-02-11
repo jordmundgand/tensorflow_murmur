619c99611c6a7f45d40c9f5e3efdfd60dafff0c1
jordmundgand <109611636+jordmundgand@users.noreply.github.com>
2024-02-12 00:14:30 +0700

    Create preprocessing.py

---
+++ tensorflow_murmur/layers/preprocessing.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+class RandomIndexing(tf.keras.layers.Layer):
+    def __init__(self, mask_value=0):
+        super().__init__()
+        def random_masked_indexing(x, mask_value=mask_value):
+            x=x!=mask_value
+            x=tf.cast(x,dtype=tf.int32)
+            x=tf.reduce_sum(x,axis=-1,keepdims=True)
+            return tf.vectorized_map(lambda y: tf.random.uniform([1], minval=0, maxval=y[0], dtype=tf.int32), x)
+        self.lambda0=tf.keras.layers.Lambda(random_masked_indexing)
+    def call(self, inputs):     
+        return self.lambda0(inputs)
+
+class LanguageMasking(tf.keras.layers.Layer):
+    def __init__(self, mask):
+        super().__init__()
+        def language_masking(x, masked_value=mask):
+            return tf.vectorized_map(lambda z: tf.tensor_scatter_nd_update(z[0], z[1][tf.newaxis,:], [masked_value]), x)
+        self.lambda0=tf.keras.layers.Lambda(language_masking)
+    def call(self, inputs, training=None):
+        masked=self.lambda0(inputs)
+        return tf.keras.backend.in_train_phase(masked, inputs[0], training=training)
+
+class IndexedSlice(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+        def indexed_slice(x):
+            return tf.vectorized_map(lambda z: tf.gather(z[0], z[1]), x)
+        self.lambda0=tf.keras.layers.Lambda(indexed_slice)
+    def call(self, inputs):
+        return self.lambda0(inputs)
