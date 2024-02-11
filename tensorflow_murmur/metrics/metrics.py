3fa03fabf38a444d1c169ec1b0f4fcb250ff7622
jordmundgand <109611636+jordmundgand@users.noreply.github.com>
2024-02-12 00:47:04 +0700

    Update metrics.py

--- tensorflow_murmur/metrics/metrics.py
+++ tensorflow_murmur/metrics/metrics.py
@@ -13,3 +13,6 @@ def masked_accuracy(label, pred):
   match = tf.cast(match, dtype=tf.float32)
   mask = tf.cast(mask, dtype=tf.float32)
   return tf.reduce_sum(match)/tf.reduce_sum(mask)
+
+def batch_average(label, pred):
+  return tf.reduce_mean(pred)
