4c5d89786f5436bd63c586e00c3cfa3c7588a5bb
jordmundgand <109611636+jordmundgand@users.noreply.github.com>
2024-10-04 14:03:16 +0700

    Create utils.py

---
+++ tensorflow_murmur/tensorflow/utils.py
@@ -0,0 +1,14 @@
+def get_strategy(platform='kaggle', device='TPU'):
+    if (platform=='kaggle') and (device=='TPU'):
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        return tf.distribute.TPUStrategy(tpu)
+    elif (platform=='kaggle') and (device=='multyGPU'):
+        strategy = tf.distribute.MirroredStrategy()
+        print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+        return strategy
+    elif (platform=='colab') and (device=='TPU'): 
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        return tf.distribute.TPUStrategy(tpu)
